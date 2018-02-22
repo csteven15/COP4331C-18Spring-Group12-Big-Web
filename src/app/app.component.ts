e77da59ec318e2c741642b21d2800600402ef851
Steven Chen <chensteven8899@gmail.com>
2018-02-22 00:51:07 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  items: Observable<any[]>;
+  title = 'app';
+  constructor(db: AngularFirestore) {
+    this.items = db.collection('items').valueChanges();
+  }
+}
