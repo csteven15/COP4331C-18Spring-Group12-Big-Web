4e25537281c3e8f0018373e1b68348aea771c8af
Akane <akanesato@Wulfric.local>
2018-04-23 11:33:15 -0400

    update (profile, map)

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -19,7 +19,8 @@ import { HttpClient } from '@angular/common/http';
 })
 export class LandingComponent implements OnInit {
   @Output() createComponent = new EventEmitter<string>();
-  images: string[] = ['https://www.ucf.edu/files/2012/08/regional-campuses.jpg', 'https://www.ucf.edu/files/2012/08/downtown.jpg', 'https://www.ucffoundation.org/view.image?Id=620'];
+  images: string[] = ['https://www.ucf.edu/files/2012/08/regional-campuses.jpg', 'https://www.ucf.edu/files/2012/08/downtown.jpg', 'https://www.ucffoundation.org/view.image?Id=620', 
+'https://today.ucf.edu/files/2013/02/IMG_9866-e1361286778150.jpg'];
 
 
   constructor(
